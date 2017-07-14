787afe1c45ff183ce23b7539dd2df8ff83f7a658
DrPandemic <bipbip500@gmail.com>
2017-07-13 21:42:04 -0400

    Adds login page

--- web/src/controllers/index.js
+++ web/src/controllers/index.js
@@ -6,3 +6,5 @@ const router = module.exports = new Router();
 
 const register = require('./register.js');
 router.use('/register', register.routes(), register.allowedMethods());
+const login = require('./login.js');
+router.use('/login', login.routes(), login.allowedMethods());
---
+++ web/src/controllers/login.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Router = require('koa-router');
+
+const router = module.exports = new Router();
+
+router.get('/', async function (ctx) {
+  /* ctx.state = { yup: 1337 };*/
+  await ctx.render('login');
+});
