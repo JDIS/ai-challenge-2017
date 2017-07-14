f741c1f6337e4da6d842681536bab0b549b1e60c
DrPandemic <bipbip500@gmail.com>
2017-07-13 20:00:22 -0400

    Default app

--- web/index.js
+++ web/index.js
@@ -1,4 +1,23 @@
 'use strict';
 
 require('dotenv').config();
-// process.env.SECRET
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+//app.use(require('koa-favicon')(require.resolve('./public/favicon.ico')));
+app.use(require('koa-logger')());
+app.use(require('koa-helmet')());
+app.use(require('koa-compress')({
+    flush: require('zlib').Z_SYNC_FLUSH
+}));
+app.keys = [process.env.SECRET];
+app.use(require('koa-session')(app));
+app.use(require('koa-bodyparser')());
+
+app.use(ctx => {
+  ctx.body = '420';
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`listening on port ${port}`);
