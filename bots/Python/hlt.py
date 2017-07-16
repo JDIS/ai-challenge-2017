16465ec8daf4748d01b27d9e3dfdde6773d25d03
jeepee12 <jeepee.12@hotmail.com>
2017-07-16 11:11:04 -0400

    Adds AStar to the Python Bot.

--- bots/Python/hlt.py
+++ bots/Python/hlt.py
@@ -1,4 +1,6 @@
 import sys
+import math
+import heapq
 from collections import namedtuple
 from itertools import chain, zip_longest
 
@@ -11,7 +13,21 @@ def grouper(iterable, n, fillvalue=None):
 
 
 NORTH, EAST, SOUTH, WEST, STILL = range(5)
+DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
 
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
 
 def opposite_cardinal(direction):
     "Returns the opposing cardinal direction."
@@ -76,6 +92,55 @@ class GameMap:
         dx = min(abs(sq1.x - sq2.x), sq1.x + self.width - sq2.x, sq2.x + self.width - sq1.x)
         dy = min(abs(sq1.y - sq2.y), sq1.y + self.height - sq2.y, sq2.y + self.height - sq1.y)
         return dx + dy
+
+    def get_direction_toward(self, sq1, sq2):
+        best_cost = math.inf
+        best_direction = None
+        for direction in DIRECTIONS:
+            cur_cost = self.get_distance(self.get_target(sq1, direction), sq2)
+            if cur_cost < best_cost:
+                best_direction = direction
+                best_cost = cur_cost
+
+        return best_direction
+
+    def get_direction_toward_with_A_star(self, sq1, sq2):
+        frontier = PriorityQueue()
+        frontier.put(sq1, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[sq1] = None
+        cost_so_far[sq1] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == sq2:
+                break
+
+            for next in self.neighbors(current):
+                new_cost = cost_so_far[current] + 1  # La valeur 1 represente le cout pour passer d'un carre a l'autre.
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + self.get_distance(sq2, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+        next_tile = sq2
+        while came_from[next_tile] != sq1: # TODO valider le !=
+            next_tile = came_from[next_tile]
+        deltaX = sq1.x - next_tile.x
+        deltaY = sq1.y - next_tile.y
+        # ((0, -1), (1, 0), (0, 1), (-1, 0), (0, 0))   # NORTH, EAST, SOUTH, WEST, STILL
+        if deltaX == -1 and deltaY == 0:
+            return WEST
+        elif deltaX == 1 and deltaY == 0:
+            return EAST
+        elif deltaX == 0 and deltaY == -1:
+            return NORTH
+        elif deltaX == 0 and deltaY == 1:
+            return SOUTH
+        else:
+            return STILL
 
 #################################################################
 # Functions for communicating with the Halite game environment  #
