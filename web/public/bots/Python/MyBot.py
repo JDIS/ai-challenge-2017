e379e93d8d51cabbbef74bc026184995c74958cb
jeepee12 <jeepee.12@hotmail.com>
2017-07-28 14:39:08 -0400

    Fix python bot

--- web/public/bots/Python/MyBot.py
+++ web/public/bots/Python/MyBot.py
@@ -8,6 +8,6 @@ hlt.send_init("MyPythonBot")
 
 while True:
     game_map.get_frame()
-    game_map.get_path(game_map.contents[0][0], game_map.contents[5][5])
+    game_map.get_direction_toward_with_A_star(game_map.contents[0][0], game_map.contents[5][5])
     moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
